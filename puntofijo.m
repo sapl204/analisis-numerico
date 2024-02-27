eeb58a8ebab21b781e058decaa5aa70739ce0498
sapl204 <sapplinas@hotmail.com>
2024-02-26 22:10:21 -0500

    mejora en punto fijo, mensajes y tabla implentada

--- puntofijo.m
+++ puntofijo.m
@@ -1,9 +1,13 @@
-function p = puntofijo(p0, tol, n, g)
-  i = 1
-  while i <= n
-    p = g(p0)
-    if abs(p - p0) < tol
-      break;
+function p = puntofijo(g,p0,N, tol)
+  i = 1;
+  fprintf("Metodo de punto fijo\n")
+  fprintf("    i    p    error\n")
+  while i <= N
+    p = g(p0);
+    fprintf("   %4.0f   %4.5f    %4.6f\n",i,p,abs(p-p0))
+    if abs(p-p0)<tol
+      fprintf("se terminaron las iteraciones")
+      break
     end
     i = i + 1;
     p0 = p;
