93c0bbd6af1622acbd6aa8feeacd28c61b7f15fd
sapl204 <sapplinas@hotmail.com>
2024-03-04 13:44:15 -0500

    programa de polinomio interpolante mejorado en python

--- python/polinomio.py
+++ python/polinomio.py
@@ -17,9 +17,11 @@ def polinomio(points, f, p0):
                 prod *= (x - points[i] )/(points[k] - points[i])
         polinomio += fun(points[k])*prod
     polinomioInter = sp.lambdify(x, polinomio)
-    tableData.append([polinomio, polinomioInter(p0)])
+    tableData.append([ polinomio, polinomioInter(p0)])
     print(tabulate(tableData, headers = heads, tablefmt = "grid"))
-
+    print("polinomio simplificado: ")
+    print(sp.simplify(polinomio))
+    sp.plot( sp.simplify(polinomio), title="polinomio interpolante")
 
 p0 = float(eval(input("ingrese el valor a evaluar en el polinomio, si requiere, usar la libreria math: ")))
 f = eval(input("Ingrese la funcion  usando sympy, ejemplo: 'sp.cos(x) + 1/x': "))
