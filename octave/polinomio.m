e773b3e09a4b0463ef49e59f31e7c03a1243794c
SEBASTIAN ALEJANDRO PEÑA LLINAS <sapllinas@hotmail.com>
2024-03-04 17:14:10 -0500

    polinomio interpolante en octave mejorado

--- octave/polinomio.m
+++ octave/polinomio.m
@@ -1,14 +1,17 @@
 function pol = polinomio(f, p0, points)
   pol = 0;
+  pkg load symbolic;
+  syms x;
   for k = 1:length(points)
     prod = 1;
     for i = 1:length(points)
       if i == k
         continue
       else
-        prod = prod*(p0 - points(i))/(points(k) - points(i));
+        prod = prod*(x - points(i))/(points(k) - points(i));
       end
     end
     pol = pol + f(points(k))*prod;
   end
+  disp(function_handle(pol)(p0));
 end
